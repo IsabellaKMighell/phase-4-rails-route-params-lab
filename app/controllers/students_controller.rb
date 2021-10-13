ed7e4af4b0ce4161aa382bd6a9d7322f59168b1e
Isabella Mighell <ikm4dm@virginia.edu>
2021-10-13 17:59:57 -0400

    commit tree e17600a23f93beb52ff7f7320c3f004595aaf831

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,27 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+      Student.by_name(params[:name])
+    else
+      Student.all 
+    end
     render json: students
+  end
+
+  def grades
+    students = Student.order(grade: :desc)
+    render json: students
+  end
+
+  def highest_grade
+    student = Student.order(grade: :desc).first
+    render json: student
+  end
+
+  def show 
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
